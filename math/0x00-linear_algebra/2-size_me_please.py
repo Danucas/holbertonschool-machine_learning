1639f7e55e6b0b2164577582d99b560004130371
Danucas <danrodcastillo1994@gmail.com>
2020-12-03 01:09:45 +0000

    fourth try - comprobations

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+
 def matrix_shape(matrix):
     """
     Calculates the shape of a matrix
     """
     return size_recursive(matrix, [])
+
 
 def size_recursive(vector, indexes):
     """
@@ -12,12 +14,13 @@ def size_recursive(vector, indexes):
     dividing in vectors
     """
     if not vector:
-        return [0]
+        indexes.append(0)
+        return indexes
     if type(vector) == list:
         indexes.append(len(vector))
         if len(vector) > 0:
             return size_recursive(vector[0], indexes)
         else:
-            return indexes
+            return size_recursive(None, indexes)
     else:
         return indexes
