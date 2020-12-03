b14adf54132f5414f6994b3b47a8e735fbb748ea
Danucas <danrodcastillo1994@gmail.com>
2020-12-03 00:56:35 +0000

    second try - recursion approach

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
+import json
+
 def matrix_shape(matrix):
     """
     Calculates the shape of a matrix
     """
-    return [len(matrix), len(matrix[0])]
+    return size_recursive(matrix, [])
+
+def size_recursive(vector, indexes):
+    """
+    iterate the matrix recusively by
+    dividing in vectors
+    """
+    if type(vector) == list and len(vector) > 0:
+        indexes.append(len(vector))
+        if :
+            return size_recursive(vector[0], indexes)
+    else:
+        return indexes
