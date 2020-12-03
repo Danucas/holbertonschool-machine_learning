a7420340c72f8295323c9182bbc61e4f36ec62a5
Danucas <danrodcastillo1994@gmail.com>
2020-12-03 06:00:46 +0000

    saddle up first try

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Return dot product
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    uses np.dot to multiply two matrices
+    """
+    return np.dot(mat1, mat2)
