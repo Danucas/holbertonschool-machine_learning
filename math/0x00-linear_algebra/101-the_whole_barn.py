74dba62c0f1d56265c71c94bb10b79e523dbf8f2
Danucas <danrodcastillo1994@gmail.com>
2020-12-04 06:40:40 +0000

    the whole barn first try

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+The whole barn, Add two Matrices
+"""
+
+
+def add_matrices(mat1, mat2):
+    """
+    Add two matrices
+    """
+    shape1 = size_recursive(mat1, [])
+    shape2 = size_recursive(mat2, [])
+    checks = [
+        len(shape1) == len(shape2),
+        *[
+            (length[0] == length[1])
+            for length in zip(shape1, shape2)
+        ]
+    ]
+    if not all(checks):
+        return None
+    new_matrix = []
+    return recursive_sum(mat1, mat2, new_matrix)
+
+
+def recursive_sum(dim1, dim2, nmat):
+    """
+    """
+    if type(dim1) == list:
+        mat = []
+        for i, mats in enumerate(zip(dim1, dim2)):
+            mat.append(recursive_sum(mats[0], mats[1], nmat))
+        return mat
+    else:
+        return dim1 + dim2
+
+
+def size_recursive(vector, indexes):
+    """
+    Iterate the matrix recusively by
+    dividing in vectors
+    @vector: one dimension on the matrix
+    @indexes: the list to return the sizes
+    """
+    if type(vector) == list:
+        indexes.append(len(vector))
+        if len(vector) > 0:
+            return size_recursive(vector[0], indexes)
+        else:
+            indexes.append(0)
+            return indexes
+    else:
+        return indexes
