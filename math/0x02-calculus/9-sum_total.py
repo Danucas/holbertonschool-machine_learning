75ee871f17861b274cbcb4c5ce756f7c3562211e
Danucas <danrodcastillo1994@gmail.com>
2020-12-10 21:59:37 +0000

    summa

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -8,7 +8,7 @@ def summation_i_squared(n):
     """
     Summa function
     """
-    if type(n) != int:
+    if type(n) != int or n < 0:
         return None
     return sum(map(
         lambda i: i ** 2,
