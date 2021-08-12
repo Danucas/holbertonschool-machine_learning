be9d32435e6bf7713d9110faa892682c1f6fe131
Danucas <danrodcastillo1994@gmail.com>
2021-08-12 13:39:23 -0500

    adding division to accuracy

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -11,5 +11,5 @@ def calculate_accuracy(y, y_pred):
     Calculates y pred accuracy
     """
     pred_check = tf.equal(y_pred, y)
-    acc_op = tf.reduce_mean(tf.cast(pred_check, tf.float32))
+    acc_op = tf.reduce_mean(tf.cast(pred_check, tf.float32) / y_pred)
     return acc_op
