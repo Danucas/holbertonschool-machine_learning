3d27a2b4598f7c052f84526fe61faa288db0f446
Danucas <danrodcastillo1994@gmail.com>
2021-08-13 01:05:40 -0500

    removing dicts

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -11,9 +11,6 @@ def create_placeholders(nx, classes):
     Defines the inputs to the neural network
     and the one-hot labels for the input data
     """
-    x = tf.placeholder(tf.float32, [None, nx])
-    y = tf.placeholder(tf.float32, [None, classes])
-    return (
-        tf.identity(x, name="x"),
-        tf.identity(y, name="y")
-    )
+    x = tf.placeholder(tf.float32, [None, nx], name='x')
+    y = tf.placeholder(tf.float32, [None, classes], name='y')
+    return x, y
