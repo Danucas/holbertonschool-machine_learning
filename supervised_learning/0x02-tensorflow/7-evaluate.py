326b9a5e092e2e19c9410b030da290f54f46b10f
Danucas <danrodcastillo1994@gmail.com>
2021-08-16 12:52:49 -0500

    evaluate model

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Plot accuracy and loss metrics
+"""
+
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    Load model and evaluate metrics
+    """
+    sess = tf.Session()
+    saver = tf.train.import_meta_graph(save_path + '.meta')
+    saver.restore(sess, './model.ckpt')
+    x = tf.get_collection('x')[0]
+    y = tf.get_collection('y')[0]
+    y_pred = tf.get_collection('y_pred')[0]
+    loss = tf.get_collection('loss')[0]
+    accuracy = tf.get_collection('accuracy')[0]
+
+    cost = sess.run(loss, feed_dict={x: X, y: Y})
+    acc = sess.run(accuracy, feed_dict={x: X, y: Y})
+    pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+
+    return pred, acc, cost
